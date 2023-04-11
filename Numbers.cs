4e3c74cf7be7a038beec946cadee5fcb519f9f6b
nizarmamk <nizarmbtp@students.amikom.ac.id>
2023-04-11 07:48:24 +0700

    Praktikum5

---
+++ Numbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndConstructor
+{
+    internal class Numbers
+    {
+        public void CariNilaiMin(int a, int b, int c)
+        {
+            Console.WriteLine("Berikut adalah daftar nilai input : {0}, {1}, {2}", a, b, c);
+            if (a <= b && a <= c)
+            {
+                Console.WriteLine("Nilai Terkecil : {0}", a);
+            }
+            else if (b <= a && b <= c)
+            {
+                Console.WriteLine("Nilai Terkecil : {0}", b);
+            }
+            else if (c <= a && c <= b)
+            {
+                Console.WriteLine("Nilai Terkecil : {0}", c);
+
+            }
+        }
+        public int CariNilaiMax(int a, int b, int c)
+        {
+            Console.WriteLine("Berikut adalah daftar nilai input : {0}, {1}, {2}", a, b, c);
+            if (a >= b && a >= c)
+            {
+                Console.WriteLine("Nilai Terbesar : {0}", a);
+            }
+            else if (b >= a && b >= c)
+            {
+                Console.WriteLine("Nilai Terbesar : {0}", b);
+            }
+            else if (c >= a && c >= b)
+            {
+                Console.WriteLine("Nilai Terbesar : {0}", c);
+            }
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using MethodsAndConstructor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers nilai = new Numbers();
+
+            nilai.CariNilaiMin(4, 8, 2);
+            nilai.CariNilaiMin(7, 1, 5);
+            nilai.CariNilaiMin(9, 9, 9);
+            Console.WriteLine("====================================================");
+            nilai.CariNilaiMax(6, 3, 9);
+            nilai.CariNilaiMax(60, 45, 30);
+            nilai.CariNilaiMax(19, 19, 19);
+            Console.ReadKey();
+        }
+    }
+}
